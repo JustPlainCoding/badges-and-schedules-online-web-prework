b4b22f5779e46b96103e5e03b902301204050bc9
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-06 22:05:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each{|attendee| badge_maker(attendee)}
+  badges = []
+  attendees.each{|attendee| badges << badge_maker(attendee)}
 end
 
 def assign_rooms
