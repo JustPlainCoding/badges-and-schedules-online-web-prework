625062f89cd0854f10cafc9a9f597d02357c2f65
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-06 22:17:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,5 +14,7 @@ def assign_rooms(speekers)
   room_assignments
 end
 
-def printer
+def printer(attendees)
+  batch_badge_creator(attendees).each{|badge| puts "#{badge}"}
+  assign_rooms(attendees).each{|room| puts "#{room}"}
 end
